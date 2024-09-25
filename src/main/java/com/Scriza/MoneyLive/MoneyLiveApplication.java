67aa45dfda6c5fa8a6aefcf689ce2827b2d3654e
ritiksoni <Ritiksoni101@gmail.com>
2024-09-25 12:17:38 +0530

    first commit

---
+++ src/main/java/com/Scriza/MoneyLive/MoneyLiveApplication.java
@@ -0,0 +1,14 @@
+package com.Scriza.MoneyLive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyLiveApplication.class, args);
+		System.out.println("MoneyLive is Live !!!!");
+	}
+
+}
---
+++ src/test/java/com/Scriza/MoneyLive/MoneyLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scriza.MoneyLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
